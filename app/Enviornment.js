0bf97d887cb4f4cf2eddae8793a492c83463bd4d
Rohith Madipelly <madipellyrohith@gmail.com>
2024-10-15 19:46:08 +0530

    Pushing

--- app/Enviornment.js
+++ app/Enviornment.js
@@ -2,8 +2,8 @@ export const APP_NAME = "Jonna's_Kitchen"
 
 
 
-export const GUEST_URL = "https://apiv2.jonnaskitchen.com"
-export const BASE_URL = "http://apiv2.jonnaskitchen.com"
+// export const GUEST_URL = "https://apiv2.jonnaskitchen.com"
+// export const BASE_URL = "http://apiv2.jonnaskitchen.com"
 
 
 export const RAZORPAY_KEY='rzp_test_8QXQb8PwGSqwjE'
@@ -19,8 +19,8 @@ export const ContactUsEmailbv='madipellirohith.123@gmail.com'
 
 
 // // Satya Local Server
-// export const GUEST_URL = "http://192.168.29.138:8080" 
-// export const BASE_URL = "http://192.168.29.138:8080"
+export const GUEST_URL = "http://192.168.29.138:8080" 
+export const BASE_URL = "http://192.168.29.138:8080"
 
 
 export const APP_LINK="https://play.google.com/store/apps/details?id=com.analogueitsolutions.Jonnas_Kitchen&hl=en_IN"
